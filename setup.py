0e3920281d6ddb1ac134708c1fd01b9a904a0ff0
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-05-29 07:27:13 +0100

    build(setup): updates build setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="melid",
-    version="0.0.1",
+    version="0.0-beta",
     author="Dev 47 - Mohammed Al Ameen",
     author_email="ameenmohammed2311@gmail.com",
-    description="A small example package",
+    description="A PyQt5 desktop application framework with simple and powerful features.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dev-47/melid",
