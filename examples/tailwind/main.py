95ab7267ae5c540327b86529407f1a9d27789c2d
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2024-10-16 18:18:13 +0100

    docs(examples/tailwind/main.py): adds tailwind config usage to sample

--- examples/tailwind/main.py
+++ examples/tailwind/main.py
@@ -4,11 +4,17 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent.parent.parent
 sys.path.append(str(BASE_DIR))
 
-from melid.tailwind import Tailwind
+from melid.tailwind import Tailwind, TailwindConfig
+
+tw_cfg = TailwindConfig()
+tw_cfg.extend(
+    colors={"primary": "orange"},
+    variants={"checked": {"type": "pseudo", "pseudo": ":checked"}},
+)
 
 print(
-    Tailwind().tw(
+    Tailwind(tw_cfg).tw(
         "Button",
-        "bg-blue-300 text-black hover:bg-blue-500 rounded-md hover:p-sm ml-sm",
+        "bg-primary border-primary text-black checked:bg-pink-500 hover:bg-blue-500 rounded-md hover:p-sm ml-sm after:w-40",
     )
 )
