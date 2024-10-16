727aa9571de56adf9f16d264978d1f2b24dd8af8
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2024-10-16 18:16:23 +0100

    feat(melid/store.py): adds stateful widget

--- melid/store.py
+++ melid/store.py
@@ -49,3 +49,37 @@ class State(QtCore.QObject):
 
     def subscribe(self, callback: typing.Callable[[typing.Any], None]):
         self.updated.connect(callback)
+
+
+class StatefulWidget:
+    def state(
+        self,
+        state: State,
+        condition: typing.Union[bool, typing.Callable[[], bool]] = True,
+        attribute: str = None,
+        params: typing.Union[typing.List, typing.Dict, typing.Callable] = None,
+    ):
+        def resolve_params(p):
+            if callable(p):
+                return resolve_params(p())
+
+            p1 = p if isinstance(p, list) else []
+            p2 = p if isinstance(p, dict) else {}
+
+            return p1, p2
+
+        def resolve_condition(c):
+            if callable(c):
+                return resolve_condition(c())
+
+            return c
+
+        def on_state_change():
+            p1, p2 = resolve_params(params)
+
+            if resolve_condition(condition) and attribute and hasattr(self, attribute):
+                getattr(self, attribute)(*p1, **p2)
+
+        state.subscribe(on_state_change)
+
+        return self
